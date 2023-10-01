705cc13fcfda7d3ece1efb6c70df1925d6535fae
aflores2002 <a3floresjr@gmail.com>
2023-09-30 19:18:33 -0700

    Implemented Swipping Screen Tabs

--- lib/tabScreens/user_details_screen.dart
+++ lib/tabScreens/user_details_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class UserDetailsScreen extends StatefulWidget {
@@ -10,7 +11,26 @@ class UserDetailsScreen extends StatefulWidget {
 class _UserDetailsScreenState extends State<UserDetailsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "User Profile",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        //automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: const Icon(
+              Icons.logout,
+              size: 30,
+            ),
+          ),
+        ],
+      ),
       body: Center(
         child: Text(
           "Welcome",
