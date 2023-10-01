675fb4f2d5e39b93ce93d08a973e3939a43d966f
Bashshar Atif <114458228+batif1@users.noreply.github.com>
2023-10-01 05:52:02 -0400

    Update mentor_screen.dart

--- lib/mentor/mentor_screen.dart
+++ lib/mentor/mentor_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-//COMMENT
 class MentorScreen extends StatefulWidget {
   const MentorScreen({Key? key}) : super(key: key);
 
@@ -17,26 +16,74 @@ class _MentorScreenState extends State<MentorScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Recomended Mentors Based on your Career Searches'),
+        title: const Text('Find your ideal mentor...'),
       ),
-      body: ListView.builder(
-          itemCount: users.length,
-          itemBuilder: (context, index) {
-            final user = users[index];
-            final name = user['name']['first'];
-            final email = user['email'];
-            //final imageUrl = user['picture']['thumbnail'];
-
-            return ListTile(
-              leading: CircleAvatar(
-                child: Text('${index + 1}'),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: const InputDecoration(
+                labelText: 'Working at',
+                hintText: 'Working at',
+                // You can customize other properties of the TextField's appearance here.
               ),
-              title: Text(name),
-              subtitle: Text(email),
-            );
-          }),
+              onChanged: (value) {
+                print('User entered: $value');
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: const InputDecoration(
+                hintText: 'as a',
+                // You can customize other properties of the TextField's appearance here.
+              ),
+              onChanged: (value) {
+                print('User entered: $value');
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: const InputDecoration(
+                labelText: 'Location',
+                hintText: 'while living in',
+                // You can customize other properties of the TextField's appearance here.
+              ),
+              onChanged: (value) {
+                print('User entered: $value');
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  final user = users[index];
+                  final name = user['name']['first'];
+                  final email = user['email'];
+                  //final imageUrl = user['picture']['thumbnail'];
+
+                  return ListTile(
+                    leading: CircleAvatar(
+                      child: Text('${index + 1}'),
+                    ),
+                    title: Text(name),
+                    subtitle: Text(email),
+                  );
+                }),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: fetchUsers,
+        onPressed: () {
+          fetchUsers();
+          print('Search button pressed');
+        },
+        child: const Icon(Icons.search),
       ),
     );
   }
